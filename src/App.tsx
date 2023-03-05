03ea227f31473e3b1d5ee213cdb20c9da717a37b
Abián Izquierdo <abizmo@gmail.com>
2023-03-05 18:23:51 +0000

    refactor(Header): make header a reusable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Link from './components/Link';
 
 function App(): JSX.Element {
   return (
     <>
-      <Header />
+      <Header>
+        <Header.Logo>
+          <svg className='w-30 h-9'>
+            <use xlinkHref='images/logo.svg#logo' />
+          </svg>
+        </Header.Logo>
+        <Header.NavWrapper>
+          <Header.Nav ariaLabel='Actions'>
+            <Header.NavItem>
+              <Link to='/login'>Login</Link>
+            </Header.NavItem>
+            <Header.NavItem>
+              <a
+                href='/sign-up'
+                className='block py-4 bg-primary-cyan text-neutral-white rounded-full px-8 lg:hover:bg-opacity-70'
+              >
+                Sign Up
+              </a>
+            </Header.NavItem>
+          </Header.Nav>
+          <Header.Nav ariaLabel='Sections'>
+            <Header.NavItem>
+              <Link to='/features'>Features</Link>
+            </Header.NavItem>
+            <Header.NavItem>
+              <Link to='/pricing'>Pricing</Link>
+            </Header.NavItem>
+            <Header.NavItem>
+              <Link to='/resources'>Resources</Link>
+            </Header.NavItem>
+          </Header.Nav>
+        </Header.NavWrapper>
+      </Header>
       <Footer name='Abián Izquierdo' url='https://www.abizmo.dev' />
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,92 +1,75 @@
-import { useState } from 'react';
+import { PropsWithChildren, useState } from 'react';
 
-function Header(): JSX.Element {
-  const [showMenu, setShowMenu] = useState(false);
-
+function Header({ children }: PropsWithChildren): JSX.Element {
   return (
     <header className='py-10 lg:py-12'>
       <div className='container mx-auto px-6'>
         <div className='flex gap-12 justify-between lg:justify-start items-center relative'>
-          <a href='/'>
-            <svg className='w-30 h-9'>
-              <use xlinkHref='images/logo.svg#logo' />
-            </svg>
-          </a>
-          <button
-            className='lg:hidden'
-            aria-controls='primary-navigation'
-            aria-expanded='false'
-            type='button'
-            onClick={() => setShowMenu((prev) => !prev)}
-          >
-            <div className='grid gap-2 w-8 h-6 grid-rows-3'>
-              <div className='w-8 bg-neutral-grayish-violet' />
-              <div className='w-8 bg-neutral-grayish-violet' />
-              <div className='w-8 bg-neutral-grayish-violet' />
-            </div>
-            <span className='sr-only'>Menu</span>
-          </button>
-          <nav
-            className={`${
-              showMenu ? 'block' : 'hidden'
-            } absolute top-full bg-primary-violet inset-x-0 mt-6 px-6 divide-y divide-neutral-white divide-opacity-40 text-neutral-white rounded-lg lg:flex-1 lg:flex lg:justify-between lg:items-center lg:bg-neutral-white lg:text-neutral-grayish-violet lg:static lg:mt-0`}
-            id='primary-navigation'
-          >
-            <ul
-              aria-label='Sections'
-              className='grid gap-4 py-8 text-center font-bold lg:flex lg:gap-10 lg:items-center'
-            >
-              <li>
-                <a
-                  href='#features'
-                  className='block py-2 lg:hover:text-neutral-dark-violet'
-                >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#pricing'
-                  className='block py-2 lg:hover:text-neutral-dark-violet'
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#resources'
-                  className='block py-2 lg:hover:text-neutral-dark-violet'
-                >
-                  Resources
-                </a>
-              </li>
-            </ul>
-            <ul
-              aria-label='Actions'
-              className='grid gap-4 py-8 text-center font-bold lg:flex lg:gap-10 lg:items-center'
-            >
-              <li>
-                <a
-                  href='/login'
-                  className='block py-2 lg:hover:text-neutral-dark-violet'
-                >
-                  Login
-                </a>
-              </li>
-              <li>
-                <a
-                  href='/sign-up'
-                  className='block py-4 bg-primary-cyan text-neutral-white rounded-full px-8 hover:bg-opacity-70'
-                >
-                  Sign Up
-                </a>
-              </li>
-            </ul>
-          </nav>
+          {children}
         </div>
       </div>
     </header>
   );
 }
+
+function HeaderLogo({ children }: PropsWithChildren) {
+  return <a href='/'>{children}</a>;
+}
+
+function HeaderNavWrapper({ children }: PropsWithChildren) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleOpenMenu = () => setShowMenu((prev) => !prev);
+
+  return (
+    <>
+      <button
+        className='lg:hidden'
+        aria-controls='navigation'
+        aria-expanded='false'
+        type='button'
+        onClick={handleOpenMenu}
+      >
+        <div className='grid gap-2 w-8 h-6 grid-rows-3'>
+          <div className='w-8 bg-neutral-grayish-violet' />
+          <div className='w-8 bg-neutral-grayish-violet' />
+          <div className='w-8 bg-neutral-grayish-violet' />
+        </div>
+        <span className='sr-only'>Menu</span>
+      </button>
+      <nav
+        className={`${
+          showMenu ? 'flex' : 'hidden'
+        } absolute top-full bg-primary-violet inset-x-0 mt-6 px-6 flex-col-reverse divide-y divide-y-reverse divide-neutral-white divide-opacity-40 text-neutral-white rounded-lg lg:flex-1 lg:flex lg:justify-between lg:flex-row-reverse lg:items-center lg:bg-neutral-white lg:text-neutral-grayish-violet lg:static lg:mt-0 lg:px-0`}
+        id='navigation'
+      >
+        {children}
+      </nav>
+    </>
+  );
+}
+
+interface HeaderNavProps {
+  ariaLabel: string;
+}
+
+function HeaderNav({ ariaLabel, children }: PropsWithChildren<HeaderNavProps>) {
+  return (
+    <ul
+      aria-label={ariaLabel}
+      className='grid gap-4 py-8 text-center font-bold lg:flex lg:gap-10 lg:items-center'
+    >
+      {children}
+    </ul>
+  );
+}
+
+function HeaderNavItem({ children }: PropsWithChildren) {
+  return <li>{children}</li>;
+}
+Header.Logo = HeaderLogo;
+Header.Nav = HeaderNav;
+Header.NavItem = HeaderNavItem;
+Header.NavWrapper = HeaderNavWrapper;
 
 export default Header;
