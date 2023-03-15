92fd0dafec489e133a2e120a42a0af45c0b5c3d1
Abián Izquierdo <abizmo@gmail.com>
2023-03-15 19:36:12 +0000

    feat(Assets): add statistics icons

--- src/assets/data/statistics.ts
+++ src/assets/data/statistics.ts
@@ -1,6 +1,7 @@
 interface StatisticsType {
   title: string;
   description: string;
+  icon: string;
 }
 
 const statistics: StatisticsType[] = [
@@ -8,16 +9,19 @@ const statistics: StatisticsType[] = [
     title: 'Brand Recognition',
     description:
       'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+    icon: 'images/icons.svg#brand',
   },
   {
     title: 'Detailed Records',
     description:
       'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+    icon: 'images/icons.svg#records',
   },
   {
     title: 'Fully Customizable',
     description:
       'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+    icon: 'images/icons.svg#customizable',
   },
 ];
 
